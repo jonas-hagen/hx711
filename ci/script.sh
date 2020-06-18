57a3a1a4200faba42f14ddf54c848f6dcf5263ea
Jonas Hagen <jonas.hagen@qrst.ch>
2020-06-18 17:00:02 +0200

    Include never_type feature when testing.

--- ci/script.sh
+++ ci/script.sh
@@ -1,10 +1,26 @@
 set -euxo pipefail
 
+run_tests() {
+    case $TRAVIS_RUST_VERSION in
+        stable)
+            cargo test
+            ;;
+        beta)
+            cargo test
+            ;;
+        nightly)
+            cargo test --features "never_type"
+            ;;
+        *)
+            ;;
+    esac
+}
+
 main() {
     cargo check --target $TARGET
     case $TARGET in
         x86_64*)
-            cargo test
+            run_tests
             ;;
         *)
             ;;
